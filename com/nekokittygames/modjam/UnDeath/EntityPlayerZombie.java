ee51f8c86cd5bceab743a14f8b481a297ccb2e45
nekosune <kat.swales@nekokittygames.com>
2013-08-02 11:45:00 +0100

    Continuing with the renderer (a lot of material is being copied from minecraft src with minor alteration)

--- com/nekokittygames/modjam/UnDeath/EntityPlayerZombie.java
+++ com/nekokittygames/modjam/UnDeath/EntityPlayerZombie.java
@@ -6,6 +6,7 @@ package com.nekokittygames.modjam.UnDeath;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 /**
@@ -16,6 +17,17 @@ public class EntityPlayerZombie extends EntityZombie {
 
 	
 	public InventoryPlayerZombie inventory;
+	private int itemInUseCount=100; //TODO: For now until I can experiment with how to deal withthis
+	private String ZombieName="";
+	
+	public String getZombieName() {
+		return ZombieName;
+	}
+
+	public void setZombieName(String zombieName) {
+		ZombieName = zombieName;
+	}
+
 	public EntityPlayerZombie(World par1World) {
 		super(par1World);
 		inventory=new InventoryPlayerZombie(this);
@@ -38,5 +50,14 @@ public class EntityPlayerZombie extends EntityZombie {
         par1NBTTagCompound.setTag("Inventory", this.inventory.writeToNBT(new NBTTagList()));
         par1NBTTagCompound.setInteger("SelectedItemSlot", this.inventory.currentItem);
     }
+	
+	public int getItemInUseCount()
+    {
+        return this.itemInUseCount;
+    }
 
+	public ResourceLocation func_110306_p() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
