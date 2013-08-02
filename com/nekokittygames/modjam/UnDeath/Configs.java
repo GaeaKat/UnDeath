6175423f624794530d1080cafa5d61a7aaf11617
nekosune <kat.swales@nekokittygames.com>
2013-08-02 01:44:36 +0100

    Continued with usual skellington dding in ability to load string cofiguration.

---
+++ com/nekokittygames/modjam/UnDeath/Configs.java
@@ -0,0 +1,50 @@
+package com.nekokittygames.modjam.UnDeath;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Field;
+
+import net.minecraftforge.common.Configuration;
+
+public class Configs {
+
+	@Retention(RetentionPolicy.RUNTIME)
+	private static @interface CfgId {
+		public boolean block() default false;
+	}
+	@Retention(RetentionPolicy.RUNTIME)
+	private static @interface CfgBool {}
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	private static @interface CfgString {}
+	
+	public static void  load(Configuration config) {
+		try {
+			config.load();
+			Field[] fields = Configs.class.getFields();
+			for(Field field : fields) {
+				CfgId annotation = field.getAnnotation(CfgId.class);
+				if(annotation != null) {
+					int id = field.getInt(null);
+					if(annotation.block()){
+						id = config.getBlock(field.getName(), id).getInt();
+					}else{
+						id = config.getItem(field.getName(), id).getInt();
+					}
+					field.setInt(null, id);
+				} else {
+					if(field.isAnnotationPresent(CfgBool.class)){
+						boolean bool = field.getBoolean(null);
+						bool = config.get(Configuration.CATEGORY_GENERAL,
+								field.getName(), bool).getBoolean(bool);
+						field.setBoolean(null, bool);
+					}
+				}
+			}
+		} catch(Exception e) {
+			//failed to load configs log
+		} finally {
+			config.save();
+		}
+	}
+}
--- com/nekokittygames/modjam/UnDeath/UnDeath.java
+++ com/nekokittygames/modjam/UnDeath/UnDeath.java
@@ -1,7 +1,16 @@
 package com.nekokittygames.modjam.UnDeath;
 
+import java.util.logging.Logger;
+
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid=UnDeath.ID,name=UnDeath.NAME,version=UnDeath.VERSION,modLanguage="java")
@@ -14,4 +23,26 @@ public class UnDeath {
 	
 	@Instance(ID)
 	public static UnDeath Instance;
+	
+	@SidedProxy(clientSide="com.nekokittygames.modjam.UnDeath.client.ClientProxy",serverSide="com.nekokittygames.modjam.UnDeath.ServerProxy")
+	public ServerProxy proxy;
+	
+	public static Logger logging;
+	
+	@EventHandler
+	public void PreInit(FMLPreInitializationEvent event)
+	{
+		logging=Logger.getLogger(ID);
+		logging.setParent(FMLLog.getLogger());
+	}
+	
+	@EventHandler
+	public void Init(FMLInitializationEvent event)
+	{
+	}
+	
+	@EventHandler
+	public void PostInit(FMLPostInitializationEvent event)
+	{
+	}
 }
