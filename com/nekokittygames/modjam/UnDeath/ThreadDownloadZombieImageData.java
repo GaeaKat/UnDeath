dcd8f84bdf59c0a645f522d1bde0705f5ef5e6f7
nekosune <kat.swales@nekokittygames.com>
2013-08-02 15:45:00 +0100

    Reimplementing some mojang classes so I can get at the data I need for this

---
+++ com/nekokittygames/modjam/UnDeath/ThreadDownloadZombieImageData.java
@@ -0,0 +1,17 @@
+package com.nekokittygames.modjam.UnDeath;
+
+import java.io.IOException;
+
+import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.resources.ResourceManager;
+
+public class ThreadDownloadZombieImageData extends AbstractTexture {
+
+	@Override
+	public void func_110551_a(ResourceManager resourcemanager)
+			throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
