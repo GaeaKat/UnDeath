0f88bbffb07114cdfcc8c36e1ee8a35096e16f0f
nekosune <kat.swales@nekokittygames.com>
2013-08-02 01:27:59 +0100

    Started proxies

--- com/nekokittygames/modjam/UnDeath/UnDeath.java
+++ com/nekokittygames/modjam/UnDeath/UnDeath.java
@@ -1,10 +1,17 @@
 package com.nekokittygames.modjam.UnDeath;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid=UnDeath.ID,name=UnDeath.NAME,version=UnDeath.VERSION)
+@Mod(modid=UnDeath.ID,name=UnDeath.NAME,version=UnDeath.VERSION,modLanguage="java")
+@NetworkMod(clientSideRequired=true,serverSideRequired=false)
 public class UnDeath {
 	public static final String ID = "UnDeath";
 	public static final String VERSION = "1.0";
 	public static final String NAME = "Un Death";
+	
+	
+	@Instance(ID)
+	public static UnDeath Instance;
 }
---
+++ com/nekokittygames/modjam/UnDeath/client/ClientProxy.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.nekokittygames.modjam.UnDeath.client;
+
+import com.nekokittygames.modjam.UnDeath.ServerProxy;
+
+/**
+ * @author Katrina
+ *
+ */
+public class ClientProxy extends ServerProxy {
+
+}
