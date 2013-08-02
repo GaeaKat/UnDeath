b611e65654d81be42e436414d6ff7e9d0a70b9f0
nekosune <kat.swales@nekokittygames.com>
2013-08-02 01:15:26 +0100

    Added basic skellington, still adding

--- com/nekokittygames/modjam/UnDeath/UnDeath.java
+++ com/nekokittygames/modjam/UnDeath/UnDeath.java
@@ -1,5 +1,10 @@
 package com.nekokittygames.modjam.UnDeath;
 
-public class UnDeath {
+import cpw.mods.fml.common.Mod;
 
+@Mod(modid=UnDeath.ID,name=UnDeath.NAME,version=UnDeath.VERSION)
+public class UnDeath {
+	public static final String ID = "UnDeath";
+	public static final String VERSION = "1.0";
+	public static final String NAME = "Un Death";
 }
