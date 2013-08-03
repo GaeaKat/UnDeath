396a456bc4d9a7e9821c39ef2370806ebca30492
nekosune <kat.swales@nekokittygames.com>
2013-08-03 20:30:00 +0100

    Started next mob

---
+++ com/nekokittygames/modjam/UnDeath/EntityPlayerSkellington.java
@@ -0,0 +1,12 @@
+package com.nekokittygames.modjam.UnDeath;
+
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.world.World;
+
+public class EntityPlayerSkellington extends EntitySkeleton {
+
+	public EntityPlayerSkellington(World par1World) {
+		super(par1World);
+	}
+
+}
