32f0b23e2b89b1bccae8b91c7f5fd83b29d1c5b1
nekosune <kat.swales@nekokittygames.com>
2013-08-04 18:15:00 +0100

    working on slime inventory

--- com/nekokittygames/modjam/UnDeath/EntityPlayerSlime.java
+++ com/nekokittygames/modjam/UnDeath/EntityPlayerSlime.java
@@ -1,9 +1,13 @@
 package com.nekokittygames.modjam.UnDeath;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
 public class EntityPlayerSlime extends EntitySlime {
@@ -31,6 +35,14 @@ public class EntityPlayerSlime extends EntitySlime {
     }
 	
 	
+	@Override
+	protected void dropEquipment(boolean par1, int par2) {
+		for(ItemStack item:items)
+		{
+			this.entityDropItem(item, 0.0f);
+		}
+	}
+
 	public void playerInit(EntityPlayer player,EntitySlime slime)
 	{
 		if(slime!=null)
@@ -38,6 +50,18 @@ public class EntityPlayerSlime extends EntitySlime {
 			this.setSlimeSize(slime.getSlimeSize());
 			slime.isDead=true;
 		}
+		
+		System.arraycopy(player.inventory.mainInventory, 0, items, 0, player.inventory.mainInventory.length);
+		System.arraycopy(player.inventory.armorInventory, 0, items, player.inventory.mainInventory.length, player.inventory.armorInventory.length);
+		ItemStack head=new ItemStack(Item.skull, 1, 3);
+		NBTTagCompound compound= head.getTagCompound();
+		if(compound==null)
+			compound=new NBTTagCompound();
+		compound.setString("SkullOwner", player.username);
+		head.setTagCompound(compound);
+		items[40]=head;
+		
 	}
+	
 	
 }
