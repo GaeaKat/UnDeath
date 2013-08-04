5380a30d6ceac08b80d2e6d40f50330b646ecff5
nekosune <kat.swales@nekokittygames.com>
2013-08-04 20:15:00 +0100

    Workin on slime rendering!

--- com/nekokittygames/modjam/UnDeath/client/ClientProxy.java
+++ com/nekokittygames/modjam/UnDeath/client/ClientProxy.java
@@ -3,7 +3,10 @@
  */
 package com.nekokittygames.modjam.UnDeath.client;
 
+import net.minecraft.client.model.ModelSlime;
+
 import com.nekokittygames.modjam.UnDeath.EntityPlayerSkellington;
+import com.nekokittygames.modjam.UnDeath.EntityPlayerSlime;
 import com.nekokittygames.modjam.UnDeath.EntityPlayerZombie;
 import com.nekokittygames.modjam.UnDeath.ServerProxy;
 
@@ -20,5 +23,6 @@ public class ClientProxy extends ServerProxy {
 	{
 		RenderingRegistry.registerEntityRenderingHandler(EntityPlayerZombie.class, new RenderPlayerZombie());
 		RenderingRegistry.registerEntityRenderingHandler(EntityPlayerSkellington.class, new RenderPlayerSkellington());
+		RenderingRegistry.registerEntityRenderingHandler(EntityPlayerSlime.class, new RenderPlayerSlime(new ModelSlime(16), new ModelSlime(0), 0.25f));
 	}
 }
