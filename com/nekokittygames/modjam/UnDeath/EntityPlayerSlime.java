686796a27bae72842279200f8b268e73ab447a4f
nekosune <kat.swales@nekokittygames.com>
2013-08-04 17:45:00 +0100

    starting on the custom slime

--- com/nekokittygames/modjam/UnDeath/EntityPlayerSlime.java
+++ com/nekokittygames/modjam/UnDeath/EntityPlayerSlime.java
@@ -1,5 +1,6 @@
 package com.nekokittygames.modjam.UnDeath;
 
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.world.World;
 
@@ -8,5 +9,20 @@ public class EntityPlayerSlime extends EntitySlime {
 	public EntityPlayerSlime(World par1World) {
 		super(par1World);
 	}
-
+	
+	@Override
+	public void setDead()
+    {
+        this.isDead = true;
+    }
+	@Override
+	protected void setSlimeSize(int par1)
+    {
+        this.dataWatcher.updateObject(16, new Byte((byte)par1));
+        this.setSize(0.6F * (float)par1, 0.6F * (float)par1);
+        this.setPosition(this.posX, this.posY, this.posZ);
+        this.func_110148_a(SharedMonsterAttributes.field_111267_a).func_111128_a((double)(par1 * par1));
+        //this.setEntityHealth(this.func_110138_aP());
+        this.experienceValue = par1;
+    }
 }
